39502c38355f7d6224fb69917c9bafe40fa019e2
Alvin <alvinardiansyah2002@gmail.com>
2025-11-17 01:25:04 +0700

    test: update DeepBookMarginToolkit tests to initialize with private key and network

--- tests/deepbook-margin-toolkit.test.ts
+++ tests/deepbook-margin-toolkit.test.ts
@@ -1,9 +1,15 @@
-import { DeepBookMarginPool } from '../src/toolkit';
+import { DeepBookMarginToolkit } from '../src/toolkit';
+import { config } from 'dotenv';
+config();
 
 describe('DeepBookMarginToolkit Tests', () => {
   it('Should successfully init the class instance', () => {
-    const marginPool = new DeepBookMarginPool();
-    expect(marginPool).toBeDefined();
-    expect(marginPool.marginPoolContract).toBeDefined();
+    const toolkit = new DeepBookMarginToolkit({
+      privateKey: process.env.PRIVATE_KEY as string,
+      network: 'testnet',
+
+    });
+    expect(toolkit).toBeDefined();
+    expect(toolkit.getAddress().length > 0).toBe(true);
   });
 });
